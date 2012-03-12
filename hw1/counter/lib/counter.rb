943cbc2714702634b9a07ed1814e828523320151
Rafael George <george.rafael@gmail.com>
2012-03-12 18:47:57 +0000

    rps simulation game assignment

--- hw1/counter/lib/counter.rb
+++ hw1/counter/lib/counter.rb
@@ -4,7 +4,7 @@ module Counter
 
   def self.count_words(str)
     str.downcase.split(/\W+/).reduce({}) do |result, word|
-      result[word] = result[word].nil? ? 1 : result[word]+1
+      result[word] += result[word].nil? ? 1 : result[word]+1
       result
     end
   end
